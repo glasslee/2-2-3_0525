7bd20ee22de8eea337aaf80c2bd9feb629bf029d
yuri <dbfl1514@likeion.org>
2019-05-26 13:40:27 +0900

    first commit of wordcount proj

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dbfl1\desktop\like lion\class lion\[django] basic\2-2,3_0525\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
